0a22089b4213e61bffad7372adedad19107c6ef9
GalacticGlum <nerftactic09@gmail.com>
2020-01-04 12:38:13 -0500

    Added more graph options

--- generate_graph.py
+++ generate_graph.py
@@ -10,6 +10,7 @@ import pandas as pd
 import scipy.spatial
 import scipy.optimize
 from pathlib import Path
+import matplotlib
 import matplotlib.pyplot as plt
 from shapely.geometry import Point
 from utils import init_logger, partition, get_geo_stepsizes
@@ -24,7 +25,19 @@ parser.add_argument('-cw', '--chunk-width', type=int, help='The number of chunks
 parser.add_argument('-ch', '--chunk-height', type=int, help='The number of chunks on the latitude (vertical). Defaults to 32.', default=32)
 parser.add_argument('--no-plot-lin-reg', dest='plot_linreg', action='store_false', help='Disables a linear regression.')
 parser.add_argument('--plot-log-reg', dest='plot_logreg', action='store_true', help='Enables a logarithmic regression.')
-parser.set_defaults(plot_linreg=True, plot_logreg=False)
+parser.add_argument('--matplotlib-style', type=str, help='The matplotlib graph style.', default='default')
+parser.add_argument('--title', type=str, help='The title of the graph.')
+parser.add_argument('--x-label', type=str, help='The label of the x-axis.', default='X')
+parser.add_argument('--y-label', type=str, help='The label of the y-axis', default='Y')
+parser.add_argument('--scatter-label', type=str, help='The scatter plot label.', default='')
+parser.add_argument('--plot-label', type=str, help='The plot label.', default='')
+parser.add_argument('--export', dest='export', help='Enable export to file.', action='store_true')
+parser.add_argument('--no-export', dest='export', help='Disable export to file.', action='store_false')
+parser.add_argument('--export-output', type=str, help='The path to the exported file.', default=None)
+parser.add_argument('--export-dpi', type=int, help='The DPI of the exported file.', default=400)
+parser.add_argument('--export-format', type=str, help='The format of the exported file.', default='png')
+parser.add_argument('--no-preview', dest='preview', help='Disable the graph preview window.', action='store_false')
+parser.set_defaults(export=False, preview=True, plot_linreg=True, plot_logreg=False)
 args = parser.parse_args()
 
 input_path = Path(args.input)
@@ -140,19 +153,20 @@ logger.setLevel(logging.INFO)
 
 for column in Y:
     y = Y[column] = np.array(Y[column])
-    plt.scatter(X, y)
+    plt.scatter(X, y, label=args.scatter_label)
 
+    plot_label_template = '{} {{}}'.format(args.plot_label).strip()
     if args.plot_linreg:
         trendline, p, rsquared = generate_polynomial_trendline(X, y)
-        plt.plot(sorted_X, trendline(sorted_X), linestyle='dashed', label='{} (linear)'.format(column))
-        logger.info('{} - R-squared (linear): {}'.format(column, round(rsquared, 3)))
+        plt.plot(sorted_X, trendline(sorted_X), linestyle='dashed', label=plot_label_template.format('{} (linear)'.format(column)))
+        logger.info(plot_label_template.format('{} - R-squared (linear): {}'.format(column, round(rsquared, 3))))
 
     if args.plot_logreg:
         log_trendline, log_p, log_rsquared = generate_curve_fit(X, y, lambda t, a, b, c: a * np.log(b * t) + c)
-        plt.plot(sorted_X, log_trendline(sorted_X), linestyle='dashed', label='{} (logarithmic)'.format(column))
-        logger.info('{} - R-squared (logarithmic): {}'.format(column, round(log_rsquared, 3)))
+        plt.plot(sorted_X, log_trendline(sorted_X), linestyle='dashed', label=plot_label_template.format('{} (logarithmic)'.format(column)))
+        logger.info(plot_label_template.format('{} - R-squared (logarithmic): {}'.format(column, round(log_rsquared, 3))))
 
-    logger.info('{} - Correlation coefficient: {}'.format(column, round(p, 3)))    
+    logger.info(plot_label_template.format('{} - Correlation coefficient: {}'.format(column, round(p, 3))))    
 
 # longitude_stepsize, latitude_stepsize = get_geo_stepsizes(points_geometry, args.chunk_width, args.chunk_height)
 # chunk_area = longitude_stepsize * latitude_stepsize
@@ -176,5 +190,33 @@ for column in Y:
         
 #         X_density.append(density)
 
+matplotlib.style.use(args.matplotlib_style)
+plt.title(args.title)
+plt.xlabel(args.x_label)
+plt.ylabel(args.y_label)
 plt.legend(loc='upper right')
-plt.show()
+
+if args.preview:
+    if not args.export:
+        plt.show()
+    else:
+        logger.warning('Graph preview was enabled but it could not be displayed since export was also enabled.' +
+            ' Previewing and exporting cannot both be enabled.')
+
+if args.export:
+    output_format = args.export_format[1:] if args.export_format.startswith('.') else args.export_format
+    if args.export_output is None:
+        output_extension = output_format
+        if output_extension == 'latex':
+            output_extension = 'tex'
+        
+        output_path = input_path.with_suffix('.export.' + output_extension)
+    else:
+        output_path = Path(args.export_output)
+    
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    if output_format == 'latex':
+        import tikzplotlib
+        tikzplotlib.save(output_path)
+    else:
+        plt.savefig(output_path, dpi=args.export_dpi)
